44ede96007ca99af46391c82f6e0f1a41141be2a
Palakgupta2002 <pg948303@gmail.com>
2023-03-06 21:13:00 +0530

    cartpage

--- cartpage.js
+++ cartpage.js
@@ -19,6 +19,7 @@ function display1(){
     div1.append(div2,div3);
     div1.setAttribute("id","mainshoppingcartnav");
     var maindiv=document.createElement("div");
+    var logintotal=document.createElement("div");
         var availableimage=document.createElement("img");
         var availabletext=document.createElement("p");
         var avaliableoffer=document.createElement("div");
@@ -58,11 +59,12 @@ function display1(){
         var main2avilable= document.createElement("div");
         mainavailable.append(avaliableoffer,ul);
         main2avilable.append( mainavailable);
-        document.getElementById("shoppingcart1").append(div1,main2avilable);
+        document.getElementById("shoppingcart1").append(div1,main2avilable,logintotal);
         mainavailable.setAttribute("id","mainavailable");
         main2avilable.setAttribute("id","main2avilable");
-
-        arr&&arr.map((ele)=>{
+      
+        mainavailable.style.marginRight="20px"
+        arr&&arr.map((ele,index)=>{
         var maincartdiv=document.createElement("div");
         var maindivmap=document.createElement("div");
         var maindivmap1=document.createElement("div");
@@ -108,28 +110,31 @@ function display1(){
         image1.src="file:///C:/Users/User/Downloads/clublogo.webp";
         image1.style.height="20px";
         image1.style.position="relative";
-        image1.style.paddingTop="15px";
+        image1.style.paddingTop="8px";
         var image1p=document.createElement("p");
         image1p.innerText="Club Price";
         var tax=document.createElement("p");
         var discountprice=document.createElement("p");
-        discountprice.innerText=ele.Rate+ Math.floor((Math.random() * 10) + 1);
+        discountprice.innerText= + ele.Rate + Math.floor((Math.random() * 10) );
         tax.innerText="MRP Includes all taxes";
         tax.style.color="#757575";
         var quantity=document.createElement("p");
         quantity.innerText="Qty:";
+        quantity.style.marginTop="20px";
         var button1=document.createElement("button")
-        button1.innerText = "+"
+        button1.innerText = "+";
         let count = 1;
         let p = document.createElement("p");
         p.innerText = 1;
+        p.style.position="relative";
+        p.style.top="5px";
         button1.addEventListener("click",(()=>{
                count++;
                p.innerText = count;
         }))
         var button2=document.createElement("button");
         button2.innerText = "-";
-        button2.addEventListener("click",(()=>{
+        button2.addEventListener("click",((ele)=>{
               if(count>1){
                      count--;
               }
@@ -154,14 +159,94 @@ function display1(){
         qty1.append(quantity,button1,p,button2);
         qty1.style.display="flex";
         qty1.style.columnGap="10px";
+        var removelikediv=document.createElement("div");
+        var imageremove=document.createElement("img");
+        imageremove.src="https://iili.io/H0SGJl1.webp";
+        imageremove.style.height="25px";
+        imageremove.style.position="reletive";
+        imageremove.style.marginTop="12px";
+        console.log(imageremove);
+        var imageremovep=document.createElement("p");
+        imageremovep .innerText="Remove";
+        var imageremovediv=document.createElement("div");
+        var likeimage=document.createElement("p");
+       likeimage.style.height="20px";
+       likeimage.style.position="relative";
+       likeimage.style.top="0px";
+       likeimage.style.left="10px";
+       likeimage.style.cursor="pointer";
+       likeimage.innerText="🤍"
+       var likeimagep=document.createElement("p");
+       likeimagep.innerText="MOVE TO SHORTLIST";
+       likeimagep.style.position="relative";
+       likeimagep.style.top="1.6px";
+       likeimagep.style.fontWeight="500";
+       let heart = false;
+       likeimage.addEventListener("click",(()=>{
+               if(heart==false){
+                     heart = true;
+                     likeimage.innerText="❤️";
+               }else{
+                     heart = false;
+                     likeimage.innerText="🤍";
+               }
+       }))
+     
+       
+        var likeimagediv=document.createElement("div");
+        likeimagediv.append(likeimage,likeimagep);
+        likeimagediv.style.display="flex";
+        likeimagediv.style.columnGap="20px";
+        imageremovediv.append(imageremove,imageremovep);
+        imageremovediv.style.display="flex";
+        imageremovediv.style.columnGap="20px";
+        imageremovediv.addEventListener("click",(()=>{
+              arr.splice(index,1);
+              localStorage.setItem("cart",JSON.stringify(arr));
+              window.location.reload();
+       }))
+        removelikediv.append(imageremovediv,likeimagediv);
+        removelikediv.style.display="flex";
+        removelikediv.style.columnGap="30px";
+        removelikediv.style.position="relative";
+        removelikediv.style.paddingTop="10px";
+        var maindivmapparent=document.createElement("div");
+        maindivmapparent.append(maindivmap, removelikediv);
         maindivmap1.append(Rate1, deletediv,clubimage,discountprice,tax, qty1);
-        maincartdiv.append(maindivmap, maindivmap1);
+       
+ 
+        maincartdiv.append( maindivmapparent, maindivmap1);
         maindivmap1.setAttribute("id","maindivmap1");
         maincartdiv.style.display="flex";
-
-
+        maincartdiv.style.borderTop="0.5px solid  rgb(218, 208, 208)";
+        maincartdiv.style.marginTop="11px";  
  })
-    
-    
-}
+ 
+    }
 display1()
+var logintotal1=document.createElement("button");
+logintotal1.innerText="login to place order";
+logintotal1.addEventListener("click",(()=>{
+     location.href="login.html";
+   
+
+}));
+let amout=0;
+var total=arr.filter((ele)=>{
+ amout=ele.Rate+amout;
+      }
+)
+var logintotaldiv=document.createElement("div");
+var totalamoutdiv=document.createElement("div");
+var totalamount=document.createElement("p");
+totalamount.innerText=amout;
+var logindiv=document.createElement("div");
+logintotal1.style.width="400px";
+logintotal1.style.marginTop="20px";
+logintotal1.style.backgroundColor="#FF7043";
+logindiv.append(logintotal1);
+logindiv.style.backgroundColor="red";
+totalamoutdiv.append(totalamount);
+logindiv.append(totalamoutdiv);
+logintotaldiv.append(logindiv,totalamoutdiv);
+document.getElementById("total").append(logindiv);
